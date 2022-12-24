fe0096e9e80538ce47e38b2f4308fc0c57d6e74a
kkulebaev <konstantinkulebaev@gmail.com>
2022-12-24 23:54:56 +0300

    add logo and some styles for login form

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,4 +1,5 @@
 import { createSignal } from 'solid-js'
+import KeyLogo from './../assets/images/key-logo.svg'
 
 export const LoginForm = () => {
   const [username, setUsername] = createSignal('')
@@ -21,13 +22,17 @@ export const LoginForm = () => {
 
   return (
     <form class='flex flex-col gap-4 p-6 b-2 b-solid b-zinc-7 b-rd-2 bg-zinc-8' onSubmit={e => onSubmitHandler(e)}>
+      <div class='flex justify-center gap-1'>
+        <img src={KeyLogo} alt='key-logo' />
+        <h1 class='text-size-2xl'>Auth Template</h1>
+      </div>
       <div class='flex flex-col gap-1'>
         <label for='username'> Username or email address </label>
-        <input id='username' class='w-xs' type='text' value={username()} name='username' required onInput={e => usernameHandler(e)} />
+        <input id='username' class='w-xs text-base p-1' type='text' value={username()} name='username' required onInput={e => usernameHandler(e)} />
       </div>
       <div class='flex flex-col gap-1'>
         <label for='password'> Password </label>
-        <input id='password' class='w-xs' type='password' value={password()} name='password' required onInput={e => passwordHandler(e)} />
+        <input id='password' class='w-xs text-base p-1' type='password' value={password()} name='password' required onInput={e => passwordHandler(e)} />
       </div>
       <button class='b-1 b-solid b-transparent bg-zinc-9 hover-b-indigo' type='submit'>
         Sign in
