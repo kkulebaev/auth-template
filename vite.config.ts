7c292f985b02fa3ebc03fcc2deed1a0637e24935
kkulebaev <konstantinkulebaev@gmail.com>
2022-12-25 14:57:34 +0300

    install solid devtools

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vite'
+import devtools from 'solid-devtools/vite'
 import solidPlugin from 'vite-plugin-solid'
 import Unocss from 'unocss/vite'
 
 export default defineConfig({
   plugins: [
+    devtools({
+      autoname: true,
+    }),
     solidPlugin(),
     Unocss({
       /* options */
