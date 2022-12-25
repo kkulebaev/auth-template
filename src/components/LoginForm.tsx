9fccba01348b39ec4c26fa22fd479cc664810167
kkulebaev <konstantinkulebaev@gmail.com>
2022-12-25 17:50:26 +0300

    add handle errors

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -6,8 +6,10 @@ export const LoginForm = () => {
   const [username, setUsername] = createSignal('')
   const [password, setPassword] = createSignal('')
 
-  const [isUsernameError, setIsUsernameError] = createSignal(false)
-  const [isPasswordError, setIsPasswordError] = createSignal(false)
+  const [error, setError] = createSignal({
+    username: '',
+    password: '',
+  })
 
   const inputHandler = (e: InputEvent, cb: Setter<string>) => {
     const el = e.target as HTMLInputElement
@@ -16,25 +18,24 @@ export const LoginForm = () => {
 
   const onSubmitHandler = (e: Event) => {
     e.preventDefault()
-    setIsPasswordError(true)
-    setIsUsernameError(true)
+    setError({ username: username(), password: password() })
   }
 
   return (
-    <form class='flex flex-col p-6 b-2 b-solid b-zinc-7 b-rd-2 bg-zinc-8' onSubmit={e => onSubmitHandler(e)}>
+    <form class='flex flex-col gap-2 p-6 b-2 b-solid b-zinc-7 b-rd-2 bg-zinc-8' onSubmit={onSubmitHandler}>
       <FormHeader />
       <div class='flex flex-col gap-1'>
         <label for='username'> Username or email address </label>
         <input id='username' class='w-xs text-base p-1' type='text' value={username()} name='username' required onInput={e => inputHandler(e, setUsername)} />
-        <span class='text-red' classList={{ 'op-0': !isUsernameError() }}>
-          Error
+        <span class='text-red' classList={{ 'op-0': !error().username }}>
+          {error().username}
         </span>
       </div>
       <div class='flex flex-col gap-1'>
         <label for='password'> Password </label>
         <input id='password' class='w-xs text-base p-1' type='password' value={password()} name='password' required onInput={e => inputHandler(e, setPassword)} />
-        <span class='text-red op-0' classList={{ 'op-0': !isPasswordError() }}>
-          Error
+        <span class='text-red' classList={{ 'op-0': !error().password }}>
+          {error().password}
         </span>
       </div>
       <button class='mt-4 b-1 b-solid b-transparent bg-zinc-9 hover-b-indigo' type='submit'>
