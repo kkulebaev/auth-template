2ae12ce4e66b7bc3e12404f75d4a4951adbfdfdf
kkulebaev <konstantinkulebaev@gmail.com>
2022-12-27 14:25:09 +0300

    restyling

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,12 +1,12 @@
 import type { Setter } from 'solid-js'
 import { createSignal } from 'solid-js'
-import { FormHeader } from './FormHeader'
 import { AppButton } from './AppButton'
 
 export const LoginForm = () => {
   const [username, setUsername] = createSignal('')
   const [password, setPassword] = createSignal('')
   const [isLoading, setIsLoading] = createSignal(false)
+  const [showPassword, setShowPassword] = createSignal(false)
 
   const DEFAULT_ERRORS = () => ({
     username: '',
@@ -34,19 +34,43 @@ export const LoginForm = () => {
     }, 2000)
   }
 
+  const toggleShowPassword = () => {
+    document.body.classList.toggle('show-password')
+    setShowPassword(!showPassword())
+  }
+
   return (
-    <form class='flex flex-col gap-2 p-6 b-2 b-solid b-zinc-7 b-rd-2 bg-zinc-8' onSubmit={onSubmitHandler}>
-      <FormHeader />
-      <div class='flex flex-col gap-1'>
-        <label for='username'> Username or email address </label>
-        <input id='username' class='w-xs text-base p-1' type='text' value={username()} name='username' required onInput={e => inputHandler(e, setUsername)} />
+    <form class='flex flex-col gap-4 p-6 b-2 b-solid b-zinc-7 b-rd-2' onSubmit={onSubmitHandler}>
+      <div class='flex flex-col gap-2'>
+        <label class='text-2xl' for='username'>
+          Username or email address
+        </label>
+        <div class='relative'>
+          <input id='username' class='text-2xl p-2' type='text' value={username()} name='username' required onInput={e => inputHandler(e, setUsername)} />
+        </div>
         <span class='text-red' classList={{ 'op-0': !error().username }}>
           {error().username}
         </span>
       </div>
       <div class='flex flex-col gap-1'>
-        <label for='password'> Password </label>
-        <input id='password' class='w-xs text-base p-1' type='password' value={password()} name='password' required onInput={e => inputHandler(e, setPassword)} />
+        <label class='text-2xl' for='password'>
+          Password
+        </label>
+        <div class='relative'>
+          <input
+            id='password'
+            class='text-2xl p-2'
+            type={showPassword() ? 'text' : 'password'}
+            value={password()}
+            name='password'
+            required
+            onInput={e => inputHandler(e, setPassword)}
+          />
+          <button type='button' class='eyeball' onClick={toggleShowPassword}>
+            <div class='eye'></div>
+          </button>
+          <div class='eye__beam'></div>
+        </div>
         <span class='text-red' classList={{ 'op-0': !error().password }}>
           {error().password}
         </span>
